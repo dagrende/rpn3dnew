b067604faf97c8cc5c98063482db977e62f18bd2
dagrende <dag.rende@find-out.se>
2018-04-08 08:52:14 +0200

    improved lighting - made light follow camera, added ambient light, adjusted material properties

--- src/ModelViewer.vue
+++ src/ModelViewer.vue
@@ -7,6 +7,8 @@
   import THREE from 'three';
   const ThreeBSP = require('three-js-csg')(THREE);
   const OrbitControls = require('three-orbit-controls')(THREE);
+  import store from './store';
+
   let setObject;
 
   export default {
@@ -17,23 +19,25 @@
     mounted() {
       let canvasContainer = this.$refs.canvasContainer;
       const scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-      camera.position.x = 2;
-      camera.position.y = 2;
-      camera.position.z = -5;
       scene.rotateX(-Math.PI / 2);
       scene.rotateY(-Math.PI);
       scene.rotateX(-Math.PI);
+
+      var camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+      camera.position.set(2, 2, -5);
 
       var renderer = new THREE.WebGLRenderer({antialias: true});
       renderer.setClearColor(0xdddddd);
       const controls = new OrbitControls(camera, canvasContainer);
 
+      const ambientLight = new THREE.AmbientLight( 0x555555 );
+      scene.add( ambientLight );
+
       const light = new THREE.PointLight(0xffffff);
-      light.position.set(30, -20, -10);
+      light.position.set(0, -5, 0);
       scene.add(light);
 
-      const material = new THREE.MeshPhongMaterial({ color: 0xdddddd, specular: 0x1a1a1a, shininess: 30, shading: THREE.FlatShading });
+      const material = new THREE.MeshPhongMaterial({ color: 0xdddddd, specular: 0x080808, shininess: 60, shading: THREE.FlatShading });
 
       function adjustForCanvasContainerSize(){
         renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
@@ -58,14 +62,19 @@
           prevObj = newMesh;
         }
         render();
+
       }
 
+      const cameraLightOffset = new THREE.Vector3(0, 0, 0);
       // render model initially and on camera movement
       function render() {
+        light.position.set(-camera.position.x, camera.position.z, camera.position.y + 1);
         renderer.render( scene, camera );
       }
       controls.addEventListener('change', render);
       render();
+
+      // store.commit('buttonCommand', 'addCube')
     },
     computed: {
       stackTop () {
@@ -74,7 +83,6 @@
     },
     watch: {
       stackTop (newStackTop, oldStackTop) {
-        console.log('stackTop', newStackTop);
         setObject && setObject(newStackTop);
       }
     }
